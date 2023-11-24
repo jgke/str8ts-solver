d90d7591f8d7effb0d69324e665230e63eface73
Jaakko Hannikainen <jgke@jgke.fi>
2023-11-24 14:26:04 +0200

    Default values

--- src/components/generator.rs
+++ src/components/generator.rs
@@ -58,15 +58,15 @@ pub fn header(props: &GeneratorProps) -> Html {
         <form class="flex border w-full mb-2 flex-col bg-light-800 dark:bg-blue-300 dark:border-blue-400 rounded p-2 max-w-[538px]" {onsubmit}>
             <label class="flex flex-col">
                 <span class="block dark:text-white">{"Puzzle size"}</span>
-                <input class="mt-4 text-black" name="size" />
+                <input class="mt-4 text-black" name="size" value="9" />
             </label>
             <label class="flex flex-col">
                 <span class="block dark:text-white">{"Empty black cell count"}</span>
-                <input class="mt-4 text-black" name="blocker_count" />
+                <input class="mt-4 text-black" name="blocker_count" value="10" />
             </label>
             <label class="flex flex-col">
                 <span class="block dark:text-white">{"Black number count"}</span>
-                <input class="mt-4 text-black" name="blocker_num_count" />
+                <input class="mt-4 text-black" name="blocker_num_count" value="5"  />
             </label>
             <label class="flex flex-col">
                 <span class="block dark:text-white">{"Target difficulty"}</span>
@@ -74,15 +74,15 @@ pub fn header(props: &GeneratorProps) -> Html {
                     <option value="1">{"Trivial"}</option>
                     <option value="2">{"Easy"}</option>
                     <option value="3">{"Medium"}</option>
-                    <option value="4">{"Hard"}</option>
-                    <option value="5">{"Settis, small fishes"}</option>
-                    <option value="6">{"Large fishes, short chains"}</option>
-                    <option value="7">{"Long chains"}</option>
+                    <option value="4" selected=true>{"Hard"}</option>
+                    <option value="5">{"(SLOW) Settis, small fishes"}</option>
+                    <option value="6">{"(VERY SLOW) Large fishes, short chains"}</option>
+                    <option value="7">{"(EXTREMELY SLOW) Long chains"}</option>
                 </select>
             </label>
             <label class="flex flex-col">
                 <span class="block dark:text-white">{"Symmetric"}</span>
-                <input type="checkbox" class="mt-4 text-black" name="symmetric" />
+                <input type="checkbox" class="mt-4 text-black" name="symmetric" checked=true />
             </label>
             <button class="p-2 mt-4 flex-grow-0 border font-bold bg-light-800 text-black dark:border-blue-400 dark:bg-blue-300 dark:text-white rounded disabled:border-transparent disabled:text-light-300 dark:disabled:text-light-300" type="submit">{"Parse"}</button>
         </form>
